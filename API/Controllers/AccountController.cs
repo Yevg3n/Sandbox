71a80e9f30d4a97222b5d05511a381304d9681a1
Yevgeny <Yevgeny.Lavrov@yandex.com>
2023-08-10 21:46:11 -0400

    Add account controller and register user endpoint

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // TODO: Take JSON instead of parameters
+        [HttpPost("register")] // POST: api/account/register?username=admin&password=admin
+        public async Task<ActionResult<User>> Register(string username, string password)
+        {
+            using var hmac = new HMACSHA512();
+            var user = new User
+            {
+                Username = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
