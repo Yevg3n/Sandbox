25d148970429f1979d47fbba8765ea34e929030e
Yevgeny <Yevgeny.Lavrov@yandex.com>
2023-08-13 22:44:08 -0400

    Clean-up

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,41 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit{
-  model: any ={};
-  loggedIn = false;
+export class NavComponent{
   
-  constructor(private AccountService: AccountService) {}
-
-  ngOnInit(): void {
-    this.getCurrentUser();
-  }
-
-  // TODO: Temporary code. To be removed.
-  getCurrentUser(){
-    this.AccountService.currentUser$.subscribe({
-      next: user => this.loggedIn = !!user,
-      error: error => console.log(error)
-    });
-  }
+  model: any = {
+    username: "",
+    password: ""
+  };
+  
+  constructor(public accountService: AccountService) {}
 
   login(){
-    this.AccountService.login(this.model).subscribe({
+    this.accountService.login(this.model).subscribe({
       next: response => {
-        console.log(response);
-        this.loggedIn = true;
+        console.log(response)
       },
       error: error => console.log(error)
     })
   }
 
   logout(){
-    this.AccountService.logout();
-    this.loggedIn = false;
+    this.accountService.logout()
   }
 }
